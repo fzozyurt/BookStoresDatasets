e0bbf3044f0a1204194ba4d491b7902d79e88006
Furkan Zeki ÖZYURT <fzozyurt@outlook.com>
2023-10-15 23:49:35 +0300

    Update Prep Location

--- Scripts/all_datasets_import.py
+++ Scripts/all_datasets_import.py
@@ -1,6 +1,8 @@
 from kaggle.api.kaggle_api_extended import KaggleApi
 
-prep_location = '../Datasets'
+prep_location = 'Datasets'
 
 kaggle.api.dataset_download_files(
     "furkanzeki/bookdataset", path=prep_location, unzip=True)
+
+print("Dosya İndirildi")
