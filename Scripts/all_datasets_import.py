65e2ab49e6ed176d399fd5e8932cf45292696948
Furkan Zeki ÖZYURT <fzozyurt@outlook.com>
2023-10-16 00:02:42 +0300

    Update Parameters

--- Scripts/all_datasets_import.py
+++ Scripts/all_datasets_import.py
@@ -1,6 +1,9 @@
 from kaggle.api.kaggle_api_extended import KaggleApi
 
-prep_location = 'Datasets'
+prep_location = 'Datasets'+
+
+api = KaggleApi()
+api.authenticate()
 
 KaggleApi.dataset_download_files(
     dataset="furkanzeki/bookdataset", path=prep_location, unzip=True)
