ce535d258650190c90684ebea606b402a1f4fa72
fzozyurt <fzozyurt@outlook.com>
2024-01-27 16:15:51 +0300

    delete and rename

--- Scripts/file_partion.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-
-def partion(direction,links=[],count=3):
-    df = pd.DataFrame(links)
-    df.reset_index(drop=True, inplace=True)
-    df["Node"] = df.index.map(lambda x: x % count + 1)
-    df.to_csv(direction+"/Kategori.csv", index=False)
--- Scripts/mongodb_ping.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-hastname="localhost"
-port=27017
-
-client=MongoClient(hastname,port)
-sonuc=client.admin.command('ping')
-
-print(sonuc)
