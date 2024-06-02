c244a43f8158fd41f2af3ff81b8febfc9ff312fd
fzozyurt <fzozyurt@outlook.com>
2024-06-02 23:33:55 +0300

    update

--- Scripts/BKM/scrape_categories.py
+++ Scripts/BKM/scrape_categories.py
@@ -1,7 +1,7 @@
 import json
 from bs4 import BeautifulSoup
 import requests
-from Scripts.data_partitioning import data_partitioning
+import math
 
 
 def scrape_categories():
@@ -20,5 +20,17 @@ def scrape_categories():
 
     data_partitioning(links)
     
+def data_partitioning(links):
+    total_jobs = 5
+    chunk_size = math.ceil(len(links) / total_jobs)
+
+    for i in range(total_jobs):
+        start = i * chunk_size
+        end = start + chunk_size
+        chunk = links[start:end]
+
+        with open(f'categories_{i + 1}.json', 'w', encoding='utf-8') as f:
+            json.dump(chunk, f, ensure_ascii=False, indent=4)
+
 if __name__ == "__main__":
     scrape_categories()
