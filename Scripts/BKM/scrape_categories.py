cd80aa5264705128e9dda77755bf044aa191ca68
fzozyurt <fzozyurt@outlook.com>
2024-06-02 18:53:52 +0300

    Update

---
+++ Scripts/BKM/scrape_categories.py
@@ -0,0 +1,23 @@
+import json
+from bs4 import BeautifulSoup
+import requests
+
+def scrape_categories():
+    links = []
+    site = 'https://www.bkmkitap.com'
+
+    # Kategori İçe Aktarma
+    url = "https://www.bkmkitap.com/kategori-listesi"
+    response = requests.get(url)
+    html_icerigi = response.content
+    soup = BeautifulSoup(html_icerigi, "html.parser")
+    kategori = soup.find_all("a", class_=["btn btn-default btn-upper"])
+    for a in range(len(kategori)):
+        links.append({'name': str(kategori[a].text), 'url': str(site+"/"+(kategori[a]['href']))})
+        print({'name': str(kategori[a].text), 'url': str(site+"/"+(kategori[a]['href']))})
+    
+    with open('categories.json', 'w',encoding='UTF-8') as f:
+            json.dump(links, f,ensure_ascii=False)
+
+if __name__ == "__main__":
+    scrape_categories()
