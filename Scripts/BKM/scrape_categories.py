4d1916b084f77d11d208c0100cf39524fa24ed97
fzozyurt <fzozyurt@outlook.com>
2024-06-04 23:19:49 +0300

    category random feature

--- Scripts/BKM/scrape_categories.py
+++ Scripts/BKM/scrape_categories.py
@@ -2,6 +2,7 @@ import json
 from bs4 import BeautifulSoup
 import requests
 import math
+import numpy as np
 
 
 def scrape_categories():
@@ -19,18 +20,32 @@ def scrape_categories():
         print({'name': str(kategori[a].text), 'url': str(site+"/"+(kategori[a]['href']))})
 
     data_partitioning(links)
-    
+
 def data_partitioning(links):
-    total_jobs = 5
-    chunk_size = math.ceil(len(links) / total_jobs)
+    # Verileri numpy array'ine çevirme ve karıştırma
+    data_array = np.array(links)
+    np.random.shuffle(data_array)
 
-    for i in range(total_jobs):
-        start = i * chunk_size
-        end = start + chunk_size
-        chunk = links[start:end]
+    # Veriyi 5 parçaya bölme
+    split_data = np.array_split(data_array, 5)
 
-        with open(f'categories_{i + 1}.json', 'w', encoding='utf-8') as f:
-            json.dump(chunk, f, ensure_ascii=False, indent=4)
+    # Parçaları ayrı JSON dosyalarına yazma
+    for i, chunk in enumerate(split_data):
+        with open(f'categories_{i + 1}.json', 'w',encoding='utf-8') as file:
+            json.dump(chunk.tolist(), file,ensure_ascii=False, indent=4)
+
+    
+# def data_partitioning(links):
+#     total_jobs = 5
+#     chunk_size = math.ceil(len(links) / total_jobs)
+
+#     for i in range(total_jobs):
+#         start = i * chunk_size
+#         end = start + chunk_size
+#         chunk = links[start:end]
+
+#         with open(f'categories_{i + 1}.json', 'w', encoding='utf-8') as f:
+#             json.dump(chunk, f, ensure_ascii=False, indent=4)
 
 if __name__ == "__main__":
     scrape_categories()
